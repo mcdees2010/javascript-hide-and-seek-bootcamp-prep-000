cf55882157f0629a7a6396bea7314d0bbaf24652
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-18 20:58:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list li')
+  
+  for( let i = 0; i < rankedLists.length; i++){
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
