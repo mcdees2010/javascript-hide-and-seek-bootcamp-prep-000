aecae2145ec6623497ecc022daf1e601656a3ac9
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-18 20:26:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list li')
   
-  for( let i = 0; i < rankedLists.length; i++)
+  for( let i = 0; i < rankedLists.length; i++){
+    
+  }
+    
 }
